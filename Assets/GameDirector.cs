f43fc3f904d772f37310f233e3ef8fafced9f355
DESKTOP-64SIFJS\山内　淳司 <atushi.yumiethu@gmail.com>
2021-09-29 08:53:31 +0900

    ゲームの一連の流れを作成

--- Assets/GameDirector.cs
+++ Assets/GameDirector.cs
@@ -2,38 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameDirector : MonoBehaviour
 {
     private GameObject dir;
+    private GameObject push;
+    private GameObject stage;
     private GameObject friend;
     private GameObject enemy;
     private GameObject panel;
     public GameObject panelGameJudge;
     string sCount;
+    string scenes;
     int shotCount;
     int fastCount;
     int endCount;
     int enemyCount;
     public GameObject root;
     private Rigidbody[] rbAll;
+    private string sceneNum;
 
     // Start is called before the first frame update
     void Start()
     {
         //必要なゲームオブジェクトを取得
         this.dir = GameObject.Find("Director");
-        friend = GameObject.Find("Friend");
-        enemy = GameObject.Find("Enemy");
+        this.friend = GameObject.Find("Friend");
+        this.enemy = GameObject.Find("Enemy");
         this.panel = GameObject.Find("GameJudge");
+        this.push = GameObject.Find("Push");
+        this.stage = GameObject.Find("Stage");
         fastCount = this.friend.transform.childCount;
 
         //ゲーム開始時の残り弾数を表示
         shotCount = 3;
         sCount = shotCount.ToString();
         this.dir.GetComponent<Text>().text = "残弾数" + sCount + "発";
-
+        scenes = SceneManager.GetActiveScene().name;
+        sceneNum = scenes.Replace("GameScenes", "");
         panelGameJudge.SetActive(false);
+        push.SetActive(false);
+
+        //現在のステージを表示
+        this.stage.GetComponent<Text>().text = "STAGE" + sceneNum;
     }
 
     private bool isSleepingAll()
@@ -72,6 +84,13 @@ public class GameDirector : MonoBehaviour
         {
             panelGameJudge.SetActive(true);
             this.panel.GetComponent<Text>().text = "GAME OVER";
+            this.panel.GetComponent<Text>().color = Color.blue;
+            push.SetActive(true);
+            this.push.GetComponent<Text>().text = "Push Retry";
+            if (Input.GetMouseButtonDown(0))
+            {
+                SceneManager.LoadScene(scenes);
+            }
         }
 
         if (isSleepingAll())
@@ -82,6 +101,15 @@ public class GameDirector : MonoBehaviour
                 {
                     panelGameJudge.SetActive(true);
                     this.panel.GetComponent<Text>().text = "GAME CLEAR";
+                    this.panel.GetComponent<Text>().color = Color.yellow;
+                    push.SetActive(true);
+                    this.push.GetComponent<Text>().text = "Push NextStage";
+
+                    int num = int.Parse(sceneNum) + 1;
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        SceneManager.LoadScene("GameScenes" + num);
+                    }
                 }
             }
 
@@ -89,6 +117,13 @@ public class GameDirector : MonoBehaviour
             {
                 panelGameJudge.SetActive(true);
                 this.panel.GetComponent<Text>().text = "GAME OVER";
+                this.panel.GetComponent<Text>().color = Color.blue;
+                push.SetActive(true);
+                this.push.GetComponent<Text>().text = "Push Retry";
+                if (Input.GetMouseButtonDown(0))
+                {
+                    SceneManager.LoadScene(scenes);
+                }
             }
         }
     }
